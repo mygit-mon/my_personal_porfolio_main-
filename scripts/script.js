79776208e25240deb0983575a461448dd8c54170
Raymond Dave Guibone <mymails.dev@gmail.com>
2024-09-27 16:43:35 +0800

    Aside Icon Added

--- scripts/script.js
+++ scripts/script.js
@@ -2,11 +2,25 @@
 const navItems = document.querySelectorAll('.aside ul li a');
 
 navItems.forEach((navbar) => {
-  navbar.addEventListener('click', function () {
+  navbar.addEventListener('click', function (event) {
+    event.preventDefault();
     navItems.forEach((link) => {
       link.classList.remove('active');
     });
 
     this.classList.add('active');
+  });
+});
+
+// Change the nav icon color when it was clicked
+const asideIcons = document.querySelectorAll('.aside-icon ul li');
+
+asideIcons.forEach((asideIcon) => {
+  asideIcon.addEventListener('click', function () {
+    asideIcons.forEach((link) => {
+      link.querySelector('a').classList.remove('active');
+    });
+
+    this.querySelector('a').classList.add('active');
   });
 });
